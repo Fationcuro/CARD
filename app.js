566503a0ec322d1d11902cb18e3112938fd8c5ec
Fation <fationcuro06@gmail.com>
2022-05-21 12:23:08 +0200

    started

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let age = 32;
 let userName = 'Fation';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
-let job = { titleL: 'Developer', place:'Albania', salary:50000 };
+let job = {  title: 'Developer', place:'Albania', salary:50000 };
 
 
 alert(hobbies [1])
+console.log(job.place)
