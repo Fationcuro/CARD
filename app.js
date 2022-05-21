178cdfc559bc1e570a12424295c7aab2ec9061df
Fation <fationcuro06@gmail.com>
2022-05-21 12:11:05 +0200

    javascript alert

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-    // This is my JavaScript code!
-    console.log('Hello World');
-    
+let age = 32;
+let userName = 'Fation';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = { titleL: 'Developer', place:'Albania', salary:50000 };
+
+
+alert(hobbies [1])
